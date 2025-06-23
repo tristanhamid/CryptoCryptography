2a14429b06e1799d145be725d3f942a5043c04eb
Tristan Hamid <tristan.hamid18@gmail.com>
2025-06-22 20:07:39 -0400

    Added cryptogaphic keys and hashing

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,6 @@
 from datetime import datetime, timezone
 from core.blockchain import get_blockchain_data
-
-class User:
-    def __init__(self, id, first_name, last_name, message, btc_price, supply, block, hash_rate, utc_time):
-        self.id = id
-        self.first_name = first_name
-        self.last_name = last_name
-        self.message = message
-        self.btc_price = btc_price
-        self.supply = supply
-        self.block = block
-        self.hash_rate = hash_rate
-        self.utc_time = utc_time
+from core.crypto_keys import *
 
 #Imaginary home page
 print("Welcome to the CryptoCryptography Project!\n")
@@ -23,11 +12,12 @@ print("Welcome to the CryptoCryptography Project!\n")
 # message = input("What is your wallet address or personalized message you would like displayed on the coin?\n")
 
 # Step 2: Get Blockchain information
-btc_price = get_blockchain_data()[0]
-supply = get_blockchain_data()[1]
-block = get_blockchain_data()[2]
-hash_rate = get_blockchain_data()[3]
-utc_time = get_blockchain_data()[4]
+data = get_blockchain_data()
+btc_price = data[0]
+supply = data[1]
+block = data[2]
+hash_rate = data[3]
+utc_time = data[4]
 
 print("Price (USD): ",btc_price)
 print("Supply (BTC): ", supply)
@@ -36,8 +26,11 @@ print("Hash Rate: ", hash_rate)
 print("Timestamp (UTC): ", utc_time)
 
 # Step 3: Create a cryptographic hash to authenticate this user transaction
-
-
-
-
+key_data = generate_key_pair()
+private_key_pem = key_data[0]
+private_key_str = key_data[1]
+public_key_pem = key_data[2]
+public_key_str = key_data[3]
+# Might not need to hash if I can put public key in metadata
+fingerprint = sha256_hash(public_key_pem)
 
